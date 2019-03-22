3d25e3c86b901c700e1dad4949d09bf61029ad2e
PEF Robert <pefrobert@hotmail.com>
2019-03-22 21:14:21 +0100

    Initialize glossary page

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 // Requis
 var gulp = require('gulp');
+var fs = require('fs');
 
 // Include plugins
 var plugins = require('gulp-load-plugins')(); // tous les plugins de package.json
 
-console.log(plugins)
+var templateData = {
+  glossary : JSON.parse(fs.readFileSync('src/data/read.glossary.json'))
+};
+
+console.log(plugins);
 
 gulp.task('css', function () {
   return gulp.src('src/css/*.css')
@@ -24,8 +29,7 @@ gulp.task('img', function () {
     .pipe(gulp.dest('www/img'));
 });
 
-
-// Tâche "minify" = minification CSS (destination -> destination)
+// Tâche "minify" = minification CSS (source -> destination)
 gulp.task('minifyCSS', function () {
   return gulp.src('www/css/*.css')
     .pipe(plugins.csso())
@@ -38,6 +42,9 @@ gulp.task('minifyCSS', function () {
 gulp.task('nunjucks', function() {
   // Gets .html and .nunjucks files in pages
   return gulp.src('src/html/*.html')
+  .pipe(plugins.data(function(file){
+    return templateData;
+  }))
   // Renders template with nunjucks
   .pipe(plugins.nunjucksRender({
       path: ['src/html/templates']
@@ -45,14 +52,6 @@ gulp.task('nunjucks', function() {
   // output files in www folder
   .pipe(gulp.dest('www'));
 });
-
-gulp.task('htmlbeautify', function(){
-	return gulp.src('www/*.html')
-	  // indent html files
-  	.pipe(plugins.htmlBeautify())
-	.pipe(gulp.dest('www'));
-});
-
 
 // Tâche "build"
 gulp.task('build', ['css', 'js', 'img', 'nunjucks']);
@@ -62,10 +61,5 @@ gulp.task('prod', ['build',  'minifyCSS']);
 
 // Tâche "watch" = je surveille *less
 gulp.task('watch', function () {
-  gulp.watch('src/css/*.css', ['build']);
-  gulp.watch('src/html/*.html', ['build']);
-  gulp.watch('src/html/templates/*.njk', ['build']);
-  gulp.watch('src/js/*.js', ['build']);
-  // gulp.watch('src/img/*.*', ['build']);
-  gulp.watch('src/html/template/*.njk', ['build']);
+  gulp.watch('src/**/*.*', ['build']);
 });
